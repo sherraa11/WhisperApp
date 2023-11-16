f460dc924648d5542afeaa9700191f1124d361d8
Shera <3bedooshera@gmail.com>
2023-11-16 09:53:56 +0200

    Initial Commit

---
+++ WhisperProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhisperProject
+//
+//  Created by Abdelrahman Shera on 16/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhisperProject/WhisperProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperProjectApp.swift
+//  WhisperProject
+//
+//  Created by Abdelrahman Shera on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhisperProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
